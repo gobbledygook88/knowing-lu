d3cdba4d46797930b6482b4f5aa4679a1eb56a84
Marcus Mo <marcusmo88@gmail.com>
2018-05-26 15:40:36 +0100

    Process station names and hide on page load
    Manual map of station name fixes included

--- src/Tubemap.js
+++ src/Tubemap.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import Utils from './Utils.js';
 import Map from './assets/tubemap.svg'
+import _manualFixes from './assets/stationNameFixes.json'
 import './Tubemap.css'
 
 const svgPanZoom = require('svg-pan-zoom');
@@ -7,24 +9,31 @@ const svgPanZoom = require('svg-pan-zoom');
 class Tubemap extends Component {
   constructor(props) {
     super(props);
+
+    this.stations = {};
+    this.correct = {};
+
     this.loadSVG();
+    // this.processStationNames();
   }
 
   loadSVG() {
+    var self = this;
     var ajax = new XMLHttpRequest();
     ajax.open("GET", Map, true);
     ajax.send();
     ajax.onload = function(e) {
-      var div = document.getElementById("map-container")
+      var div = document.getElementById("map-container");
       div.innerHTML = ajax.responseText;
-      document.body.insertBefore(div, document.body.childNodes[0]);
+      // document.body.insertBefore(div, document.body.childNodes[0]);
 
       // hideText();
+      self.processStationNames();
       // _total = displayTotal();
       //
       // _svgEl = document.getElementById("status-map");
 
-      this.svg = svgPanZoom('#status-map', {
+      self.svg = svgPanZoom('#status-map', {
         center: true,
         fit: true,
         mouseWheelZoomEnabled: true,
@@ -33,7 +42,37 @@ class Tubemap extends Component {
       });
 
       // Set initial zoom level
-      this.svg.zoomBy(2);
+      self.svg.zoomBy(2);
+    }
+  }
+
+  processStationNames() {
+    var stationNames = document.getElementById("station-names");
+
+    // Loop through each child of the group and extract station names
+    // This is a HTMLCollection - an array-like object so no forEach()
+    var stations = stationNames.children;
+    for(var i = 0; i < stations.length; i++) {
+      var station = stations[i];
+      var stationName = Utils.normalise(
+        station.textContent
+               .trim()
+               .split("\n")
+               .map(w => w.trim())
+               .join(" ")
+      );
+
+      if(!stationName) { continue; }
+
+      // Manual fixes
+      stationName = _manualFixes[stationName] || stationName;
+
+      // Support multiple labels for each station
+      this.stations[stationName] = this.stations[stationName] || [];
+      this.stations[stationName].push(station);
+
+      // Hide group
+      station.setAttribute("display", "none");
     }
   }
 
