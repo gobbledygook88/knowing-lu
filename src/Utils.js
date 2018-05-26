d1acc379b9cd40c025c221fb52ef53ff3f4a14a8
Marcus Mo <marcusmo88@gmail.com>
2018-05-26 18:39:54 +0100

    Added inView method to Utils

--- src/Utils.js
+++ src/Utils.js
@@ -1,4 +1,13 @@
 class Utils {
+  static inView(elDims, vbDims) {
+    return (
+      elDims.bottom <= vbDims.bottom &&
+      elDims.left >= vbDims.left &&
+      elDims.right <= vbDims.right &&
+      elDims.top >= vbDims.top
+    );
+  }
+
   static normalise(name) {
     return name.toLowerCase()
                .replace(/[^\w\s]/gi, "")  // Remove punctuation
